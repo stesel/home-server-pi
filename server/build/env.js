e8181063654b52cdc01423700cbc7caf9efe03c9
Leonid Trofymchuk <stesel23@gmail.com>
2019-03-18 02:39:12 +0200

    HSP-00 clean up build

--- server/build/env.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = (request, response) => {
-    response.send(response.app.get("env"));
-};
-//# sourceMappingURL=env.js.map
