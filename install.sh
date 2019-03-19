241afda17c67cf84d96d604f29ceff342d0c3f50
Leonid Trofymchuk <ltrofymchuk@evolutiongaming.com>
2019-03-19 16:37:25 +0200

    HSP-00 Update install bash

--- install.sh
+++ install.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 # copy script to pi to directory where you want to install home-server-pi
 
+set -e
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
 REPOSRC="https://github.com/stesel/home-server-pi/archive/master.zip"
 NAME="home-server-pi"
+ROOTDIR=$(pwd)
 
-set -e
+CHECK_VERSION() {
+    if ! type $1 &> /dev/null
+    then
+        echo -e "${RED}Error. Please install $1${NOCOLOR}"
+        exit $?
+    fi
+    echo -e "${GREEN}$1 $($1 --version) ${NOCOLOR}"
+}
+
+INSTALL_PACKAGE() {
+    local PACKAGE=$1
+    cd ${ROOTDIR}/${NAME}/${PACKAGE}
+    yarn
+    yarn build
+    echo "${PACKAGE^^} INSTALLED"
+}
+
+CHECK_VERSION node
+CHECK_VERSION yarn
 
 if [ -d $NAME ]
 then
@@ -16,7 +41,7 @@ echo DOWNLOAD $NAME
 wget -O $NAME.zip $REPOSRC --show-progress
 if [ $? -eq 0 ]
 then
-    echo DOWNLOADED
+    echo -e "${GREEN}DOWNLOADED${NOCOLOR}"
 
     unzip $NAME.zip
 
@@ -26,24 +51,18 @@ then
     mv -f $NAME-master $NAME
     echo RENAMED TO $NAME
 
-    cd ./$NAME
-    ROOTDIR=$(pwd)
-
-    cd $ROOTDIR/client
-    yarn
-    yarn build
-    echo CLIENT INSTALLED
-
-    cd $ROOTDIR/server
-    yarn
-    yarn build
-    echo SERVER INSTALLED
+    INSTALL_PACKAGE client
+    INSTALL_PACKAGE server
 
     cd $ROOTDIR
     chmod +x $ROOTDIR/start.sh
     update-rc.d $ROOTDIR/start.sh
-    echo ADDED TO AUTOSTART
+    echo -e "${GREEN}ADDED TO AUTOSTART${NOCOLOR}"
 else
-    echo FAIL
+    echo - e "${RED}FAIL TO INSTALL ${NAME^^}${NOCOLOR}"
     exit $?
 fi
+
+echo -e "${GREEN}SUCCESS${NOCOLOR}"
+
+exit 0
